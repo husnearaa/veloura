8511495ed792a618625248b9a60b2768ce2859fc
husnearaa <arahusne667@gmail.com>
2026-03-29 16:49:11 +0600

    navbar has updated

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -51,7 +51,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white py-4 border-b border-gray-100 sticky top-0 z-50 shadow-sm">
+    <nav className="bg-white py-6 border-b border-gray-100 sticky top-0 z-50 shadow-sm">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           {/* LOGO */}
