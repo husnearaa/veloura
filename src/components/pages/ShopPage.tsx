3f215c3f5ccb07946d1822dbec56e85ab9b96c8c
husnearaa <arahusne667@gmail.com>
2026-03-31 13:57:02 +0600

    shop page has updated

--- src/components/pages/ShopPage.tsx
+++ src/components/pages/ShopPage.tsx
@@ -331,7 +331,7 @@ export default function ShopPage() {
 
           {/* Products */}
           <div className="flex-1">
-            <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-4 md:gap-x-5 md:gap-y-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-4 md:gap-x-5 md:gap-y-5">
               {filteredProducts.map((product) => (
                 <div
                   key={product.id}
