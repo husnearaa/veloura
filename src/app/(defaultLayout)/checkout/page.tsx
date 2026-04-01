3f7ffecc5d0b17ed224557349488add2cc6777b9
husnearaa <arahusne667@gmail.com>
2026-04-01 10:58:09 +0600

    checkout page has creatded

--- src/app/(defaultLayout)/checkout/page.tsx
+++ src/app/(defaultLayout)/checkout/page.tsx
@@ -1,9 +1,10 @@
+import CheckoutPage from '@/components/pages/checkout/CheckoutPage';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            
+          <CheckoutPage />  
         </div>
     );
 };
